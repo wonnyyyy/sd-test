04bca726bf31b8d0c1697b4b09facdd7e7cc3193
hyewon_raven_lee[ 이혜원 ] <hyewon.raven.lee@linecorp.com>
2023-02-08 17:11:40 +0900

    feature/test_generic_webhook_trigger (#2)
    add nexus deploy task

--- src/main/java/com/cde/sample/CdeSampleApplication.java
+++ src/main/java/com/cde/sample/CdeSampleApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CdeSampleApplication {
-//    public String getGreeting() {
-//        return "Hello World!";
-//    }
+   public String getGreeting() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
-        //System.out.println(new CdeSampleApplication().getGreeting());
+        System.out.println(new CdeSampleApplication().getGreeting());
         SpringApplication.run(CdeSampleApplication.class, args);
     }
 
